68a359e0a9656bb11d95b4d7929e14a3366458e4
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2024-01-08 19:52:58 +0900

    試しに長方形を作成

--- build/app.js
+++ build/app.js
@@ -2,3 +2,9 @@ require('./css/app');
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+
+function loop() {
+
+    ctx.fillStyle = "#ccc";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
